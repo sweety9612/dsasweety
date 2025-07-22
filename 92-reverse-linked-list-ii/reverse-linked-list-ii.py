9d9035714313ea9b44566437ef0936e3339f6790
sweety9612 <78254092+sweety9612@users.noreply.github.com>
2025-07-22 12:47:08 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (66.23%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.py
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.py
@@ -1,57 +1,26 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution(object):
     def reverseBetween(self, head, left, right):
-        if not head:
-            return
-        if not head.next:
+        if not head or left == right:
             return head
-        if left == right:
-            return head
-        leftnode=ListNode(0,None)
-        prev=None
-        cur=head
-        nextnode=head.next 
-        revhead=ListNode(0,None)
-        flag=0
-        count=0
-        while(nextnode):
-            count+=1
-
-            if count == left:
-                revhead=cur
-                flag=1
-            if flag:
-                cur.next=prev
-                prev=cur
-
-            if count == right:
-                
-                leftnode.next=cur
-                revhead.next=nextnode
-                if left == 1:
-                    head=cur
-                return head
-
-            if not flag:
-                leftnode=cur
-            cur=nextnode
-            nextnode=nextnode.next
-        cur.next=prev
-        if left==1:
-            head=cur
-        else:
-            leftnode.next=cur
-            revhead.next=nextnode
-
-        print(head)
         
-        return head
-            
-
-                
-
+        dummy = ListNode(0)
+        dummy.next = head
+        prev = dummy
         
+        # Move `prev` to the node just before `left`
+        for _ in range(left - 1):
+            prev = prev.next
+        
+        # `start` will point to the first node of the sublist to reverse
+        # `then` is the node that will be reversed
+        start = prev.next
+        then = start.next
+
+        # Reverse the sublist using head insertion technique
+        for _ in range(right - left):
+            start.next = then.next
+            then.next = prev.next
+            prev.next = then
+            then = start.next
+        
+        return dummy.next
