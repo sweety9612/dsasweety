da6dd8865866b15e030ef0243d5324f403db1409
sweety9612 <78254092+sweety9612@users.noreply.github.com>
2025-07-28 08:51:05 +0530

    Time: 18 ms (38.82%) | Memory: 12.7 MB (16.59%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,26 +1,27 @@
 class Solution(object):
     def nextGreaterElement(self, nums1, nums2):
         arr=[]
-        res=[]
+        res=[0]*len(nums1)
+        nxtgreater={}
         nums2=nums2[::-1]
-        for i in nums1:
-            j=0
-            arr=[]
-            while nums2[j]!=i:
-                arr.append(nums2[j])
-                j+=1
-
-            val=-1
-            
-            while arr and arr[-1]<=i:
+        for i in range(len(nums2)):
+            while len(arr)>0 and  nums2[i]>=arr[-1]:
                 arr.pop()
-
-            if arr and arr[-1]>i:
-                val=arr[-1]
-            res.append(val)
-        
+            if len(arr)>0:
+                nxtgreater[nums2[i]]=arr[-1]
+            else:
+                nxtgreater[nums2[i]]=-1
+            arr.append(nums2[i])
+        print(nxtgreater)
+        for j in range(len(nums1)):
+            res[j]=nxtgreater.get(nums1[j])
         return res
 
+            
 
-        
-        
+            
+            
+            
+            
+                
+
