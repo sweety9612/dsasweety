977bbf5c84af521078fde44a333181cd356a5ed6
sweety9612 <78254092+sweety9612@users.noreply.github.com>
2025-10-28 14:28:15 +0530

    Time: 3 ms (86.66%) | Memory: 12.8 MB (69.17%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -4,11 +4,11 @@ class Solution(object):
         j=len(numbers)-1
         while(i<j):
             tot=numbers[i]+numbers[j]
-            if tot<target:
-                i+=1
-            elif tot>target:
+            if tot>target:
                 j-=1
+            elif tot<target:
+                i+=1
             else:
                 return [i+1,j+1]
-        return
+
         
