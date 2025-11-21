47f890f460eff082230f4e7f61b9703653b814e3
sweety9612 <78254092+sweety9612@users.noreply.github.com>
2025-11-21 06:15:51 +0530

    Time: 43 ms (58.04%) | Memory: 13.6 MB (43.75%) - LeetSync

--- 1185-find-in-mountain-array/find-in-mountain-array.py
+++ 1185-find-in-mountain-array/find-in-mountain-array.py
@@ -2,125 +2,49 @@ class Solution(object):
     def findInMountainArray(self, target, mountainArr):
         n=mountainArr.length()
         low=0
-        high=mountainArr.length()-1
-        increasingslope=0
-        decreasingslope=0
+        high=n-1
         peak=0
-        peakflag=0
-        res=-1
-        resflag=0
-
         while(low<=high):
-            
             mid=(low+high)//2
             num=mountainArr.get(mid)
             if mid<n-1:
                 nextval=mountainArr.get(mid+1)
-            lowval=mountainArr.get(low)
-            highval=mountainArr.get(high)
             if mid>0:
                 prevval=mountainArr.get(mid-1)
-            print('low:',low,'high',high,'mid:',mid)
-            if not peakflag and num>nextval and num>prevval:
-                peak=1
-                peakflag=1
-            elif num<nextval:
-                print(num,nextval)
-                increasingslope=1
-                decreasingslope=0
-                peak=0
-            else:
-                decreasingslope=1
-                increasingslope=0
-                peak=0
-            print('decreasingslope',decreasingslope)
-            print('increasingslope',increasingslope)
-            print('peak',peak)
-            if peak:
-                if num==target:
-                    return mid
-                else:
-                    
-                    if lowval>target and highval>target:
-                        return -1
-                    elif lowval==target:
-                        return low
-                    elif highval==target:
-                        return high
-
-                    elif highval<=target and lowval<=target:
-                        lowval=mountainArr.get(low+1)
-                        highval=mountainArr.get(high-1)
-                        if lowval==target:
-                            return low+1
-                        elif highval == target:
-                            res=high-1
-                            high=mid-1
-                        elif lowval<target:
-                            high=mid-1
-                        else:
-                            low=mid+1
-                            
-
-                    elif highval<=target:
-                        low=mid+1
-                    elif lowval<=target:
-                        high=mid-1
-                    else:
-                        high=mid-1
-                print('low:',low,'high',high,'mid:',mid)
-            elif increasingslope:
-                if num==target:
-                    if not resflag:
-                        res=mid
-                        resflag=1
-                    else:
-                        res=min(res,mid)
-                    if lowval>num:
-                        return res
-                    else:
-                        high=mid-1
-                elif num>target:
-                    if lowval>target:
-                        return res
-                    else:
-                        if lowval<target and highval<target:
-                            lowval=mountainArr.get(low+1)
-                            highval=mountainArr.get(high-1)
-                            if lowval==target:
-                                return low+1
-                            elif highval == target:
-                                res=high-1
-                                
-
-                        high=mid-1
+            if mid>0 and mid<n-1:
+                if num>nextval and num>prevval:
+                    peak=mid
+                    break
+                elif num>nextval:
+                    high=mid-1
                 else:
                     low=mid+1
             else:
-                print(low,high,res,num,target,'decres')
-                if num==target:
-                    if not resflag:
-                        res=mid
-                        resflag=1
-                    else:
-                        res=min(res,mid)
-                    if lowval<=num:
-                        high=mid-1
-                    else:
-                        return res
-                elif num>target:
-                    if lowval>target:
-                        low=mid+1
-                    else:
-                        high=mid-1
-                        
-                        
-                else:
-                    high=mid-1
-        return res
-
-
-                
-                    
-
+                break
         
+        low=0
+        high=peak
+        print(peak)
+        while(low<=high):
+            mid=(low+high)//2
+            num=mountainArr.get(mid)
+            if num==target:
+                return mid
+            elif num>target:
+                high=mid-1
+            else:
+                low=mid+1
+        
+        low=peak+1
+        high=n-1
+        while(low<=high):
+            mid=(low+high)//2
+            num=mountainArr.get(mid)
+            if num==target:
+                return mid
+            elif num>target:
+                low=mid+1
+            else:
+                high=mid-1
+
+        return -1
