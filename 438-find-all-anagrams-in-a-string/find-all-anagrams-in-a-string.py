56733ff539a32ce2547227cc60f460fc1f5b1850
sweety9612 <78254092+sweety9612@users.noreply.github.com>
2025-12-02 15:07:35 +0530

    Time: 48 ms (69.00%) | Memory: 13.6 MB (32.14%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,14 +1,37 @@
 class Solution(object):
     def findAnagrams(self, s, p):
-        p_sorted = sorted(p)
-        k = len(p)
-        res = []
-        for start in range(len(s) - k + 1):
-            window = s[start:start + k]
-            if sorted(window) == p_sorted:
-                res.append(start)  
+        n,m=len(s),len(p)
+        res=[]
+        if m>n:
+            return []
+        countp={}
+        for i in p:
+            countp[i]=countp.get(i,0)+1
+        window={}
+        for i in range(m):
+            window[s[i]]=window.get(s[i],0)+1
+
+        if window==countp:
+            res.append(0)
+        
+        for i in range(m,len(s)):
+            window[s[i]]=window.get(s[i],0)+1
+  
+            left=s[i-m]
+            window[left]-=1
+            if window[left] == 0:
+                del window[left]
+            if window == countp:
+                res.append(i-m+1)
         return res
 
+            
 
+            
+                
+                
 
+            
+
+        
         
