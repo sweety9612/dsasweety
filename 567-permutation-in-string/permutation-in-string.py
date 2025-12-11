4474c726124379d2c21e3ba371ac24ed2d12e0d5
sweety9612 <78254092+sweety9612@users.noreply.github.com>
2025-12-11 05:37:37 +0530

    Time: 27 ms (63.30%) | Memory: 12.6 MB (91.23%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,17 +1,33 @@
-from collections import Counter
-
 class Solution(object):
     def checkInclusion(self, s1, s2):
-        k=len(s1)
-        s1Count=Counter(s1)
-        window=Counter(s2[:k-1])
-        for start in range(k-1,len(s2)):
-            window[s2[start]]+=1
-            if window == s1Count:
-                return True
-            window[s2[start-k+1]]-=1
-            if  window[s2[start-k+1]] == 0:
-                del  window[s2[start-k+1]]
-        return False
+        dicts1={}
+        for i in s1:
+            dicts1[i]=dicts1.get(i,0)+1
         
+        i=0
+        j=len(s1)-1
+        if len(s1)>len(s2):
+            return False
+        k,word=0,{}
+        while k<=j:
+            word[s2[k]]=word.get(s2[k],0)+1
+            k+=1
+        if word==dicts1:
+            return True
+        j+=1
+        while j<len(s2):
+            v=s2[j]
+            word[v]=word.get(v,0)+1
+            word[s2[i]]=word.get(s2[i],0)-1
+            if  word[s2[i]]<=0 :
+                del word[s2[i]]
+            i+=1
+            j+=1
+            
+            if word == dicts1:
+                return True
+        return False
+
+
+
         
