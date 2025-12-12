78de302c6851697569ee63f2246f11dcb475fa2f
sweety9612 <78254092+sweety9612@users.noreply.github.com>
2025-12-12 17:29:59 +0530

    Time: 14 ms (58.79%) | Memory: 20.2 MB (36.47%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -1,39 +1,15 @@
 class Solution(object):
     def minSubArrayLen(self, target, nums):
-        subsum=0
-        cursum=nums[0]
-        start=0
-        maxlen=len(nums)
-        curlen=0
-        end=0
-        while end < len(nums) and start<len(nums):
-            
-            if cursum == target:
-                print(target," end ",end," start ",start)
-                curlen=(end-start)+1
-                if curlen<maxlen:
-                    maxlen=curlen
-                cursum-=nums[start]
-                start+=1
-                
-            elif cursum>target:
-                while start < len(nums) and cursum >target :
-                    curlen=(end-start)+1
-                    if curlen<maxlen:
-                        maxlen=curlen
-                    cursum-=nums[start]
-                    start+=1
-                
-            else:
-                end+=1
-                if end < len(nums):
-                    cursum+=nums[end]
-                
-            print(cursum,' cursum')
-        if curlen<maxlen:
-            maxlen=curlen
-        return maxlen
-
+        left=0
+        min_len=float('inf')
+        window_sum=0
+        for right in range(len(nums)):
+            window_sum+=nums[right]
+            while window_sum>=target:
+                min_len=min(min_len,right-left+1)
+                window_sum-=nums[left]
+                left+=1
+        return 0 if min_len==float('inf') else min_len
 
 
         
