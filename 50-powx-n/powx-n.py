d9f128c5f419de252551c7fa83cbf0ad4387be12
sweety9612 <78254092+sweety9612@users.noreply.github.com>
2026-03-05 15:10:46 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (62.43%) - LeetSync

---
+++ 50-powx-n/powx-n.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def myPow(self, x, n):
+        def power(x, n):
+            if n == 0:
+                return 1
+            
+            half = power(x, n // 2)
+            
+            if n % 2 == 0:
+                return half * half
+            else:
+                return half * half * x
+        
+        if n < 0:
+            x = 1/x
+            n = -n
+        
+        return power(x, n)
