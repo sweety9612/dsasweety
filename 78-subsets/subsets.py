adaaa117cedbb48487efc04590d44cd634ffb1b2
sweety9612 <78254092+sweety9612@users.noreply.github.com>
2026-03-09 11:26:53 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (52.55%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,15 +1,16 @@
 class Solution(object):
     def subsets(self, nums):
         res=[]
-        def help(start,path):
-            res.append(path[:])
+        def help(start,arr):
+            if len(nums)==0:
+                return
+            res.append(arr[:])
             for i in range(start,len(nums)):
-                path.append(nums[i])
-                help(i+1,path)
-                path.pop()
-                
+                arr.append(nums[i])
+                help(i+1,arr)
+                arr.pop()
         help(0,[])
         return res
 
-
+        
         
