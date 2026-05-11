ac3c524bc49a4de917483a985c24ac883a6039ce
sweety9612 <78254092+sweety9612@users.noreply.github.com>
2026-05-11 16:37:57 +0530

    Time: 887 ms (5.00%) | Memory: 22 MB (26.66%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,29 +1,24 @@
 class Solution(object):
     def productExceptSelf(self, nums):
-        prodval=1
-        arr=[]
-        flag=1
-        nonzero=False
-        countzero=0
-        for i in nums:
-            if i!=0:
-                prodval*=i
-                nonzero=True
-            else:
-                flag=0
-                countzero+=1
-        if not nonzero:
-            prodval=0
-        if countzero>1:
-            prodval=0
-        for i in nums:
-            if i !=0 and flag:
-                arr.append(prodval/i)
-            elif i==0:
-                arr.append(prodval)
-                flag=0
-            elif flag==0:
-                 arr.append(0)
+        prefixarr=[1]*len(nums)
+        suffixarr=[1]*len(nums)
+        prev=1
+        for i in range(len(nums)):
+            prefixarr[i]=prev
+            prev*=nums[i]
+        prev=1
+        print(prefixarr)
+        
+        for i in range(len(nums)-1,-1,-1):
+            suffixarr[i]=prev
+            prev*=nums[i]
+        print(suffixarr)
+        for i in range(len(nums)):
+            nums[i]=prefixarr[i]*suffixarr[i]
+        return nums
 
-        return arr
+            
+
+            
+
         
