6e7ebc0efda63b2f592c19fef3b6492e00ff28bc
sweety9612 <78254092+sweety9612@users.noreply.github.com>
2026-05-28 15:39:40 +0530

    Time: 43 ms (33.86%) | Memory: 12.4 MB (98.16%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,32 +1,32 @@
+from collections import Counter
+from collections import defaultdict
 class Solution(object):
     def checkInclusion(self, s1, s2):
-        dicts1={}
-        for i in s1:
-            dicts1[i]=dicts1.get(i,0)+1
-        
-        i=0
-        j=len(s1)-1
         if len(s1)>len(s2):
             return False
-        k,word=0,{}
-        while k<=j:
-            word[s2[k]]=word.get(s2[k],0)+1
-            k+=1
-        if word==dicts1:
+        s1count=Counter(s1)
+        windowsize=len(s1)
+        j=windowsize
+        subword=s2[:len(s1)]
+        dicts2=Counter(subword)
+        print(dicts2,'dfsf')
+        if dicts2==s1count:
             return True
-        j+=1
+
         while j<len(s2):
-            v=s2[j]
-            word[v]=word.get(v,0)+1
-            word[s2[i]]=word.get(s2[i],0)-1
-            if  word[s2[i]]<=0 :
-                del word[s2[i]]
-            i+=1
-            j+=1
-            
-            if word == dicts1:
+            dicts2[s2[j]]+=1
+            left=s2[j-windowsize]
+            dicts2[left]-=1
+            if dicts2[left]<=0:
+                del dicts2[left]
+            if dicts2==s1count:
                 return True
+            j+=1
+
         return False
+
+
+        
 
 
 
